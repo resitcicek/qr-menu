61699ecb8740c342304e1bd9f5303497c719519e
Reschit <me@resitcicek.com>
2022-07-01 17:02:56 +0300

    last

--- Public/Cache/7a5c5595bd31681fa834a57750744bb832742ba2.php
+++ Public/Cache/7a5c5595bd31681fa834a57750744bb832742ba2.php
@@ -7,7 +7,7 @@
         <div class="col-12 categoryList">
             <?php $__currentLoopData = $categories; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $category): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
                 <a href="<?php echo e(url('category',['username'=>$user->username,'id'=>$category->id])); ?>">
-                    <div class="col-6 category">
+                    <div class="col-12 category">
                         <img src="<?php echo e(env('URL')); ?>/Public/Images/<?php echo e($category->image); ?>" class="categoryimage"/>
                         <span class="categoryname"><?php echo e($category->title); ?></span>
                     </div>
